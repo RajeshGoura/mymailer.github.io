b70dd5676a64cce13918111f3e2a1dbd0a21a1e6
RajeshGoura <113156193+RajeshGoura@users.noreply.github.com>
2023-04-18 00:59:29 +0530

    changed code

--- send-email.php
+++ send-email.php
@@ -1,20 +1,45 @@
 <?php
-// Get form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
 
-// Set email recipient
-$to = "210301120111@cutm.ac.in";
+$errors = [];
+$errorMessage = '';
 
-// Set email subject and message
-$email_subject = "New message from $name: $subject";
-$email_message = "Name: $name\nEmail: $email\n\n$message";
+if (!empty($_POST)) {
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
 
-// Send email
-mail($to, $email_subject, $email_message);
+   if (empty($name)) {
+       $errors[] = 'Name is empty';
+   }
 
-// Redirect to thank-you page
-header("Location: thank-you.html");
-?>
+   if (empty($email)) {
+       $errors[] = 'Email is empty';
+   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $errors[] = 'Email is invalid';
+   }
+
+   if (empty($message)) {
+       $errors[] = 'Message is empty';
+   }
+
+   if (empty($errors)) {
+       $toEmail = '210301120111@cutm.ac.in';
+       $emailSubject = 'New email from your contact form';
+       $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=utf-8'];
+       $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+       $body = join(PHP_EOL, $bodyParagraphs);
+
+       if (mail($toEmail, $emailSubject, $body, $headers)) {
+
+           header('Location: thank-you.html');
+       } else {
+           $errorMessage = 'Oops, something went wrong. Please try again later';
+       }
+
+   } else {
+
+       $allErrors = join('<br/>', $errors);
+       $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+   }
+}
+ ?>
